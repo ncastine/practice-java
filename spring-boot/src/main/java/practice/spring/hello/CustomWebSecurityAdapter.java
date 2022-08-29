b30400e0b6418944afdcf05b5a36738027c33626
Neil Castine <neil.castine@gmail.com>
2022-08-28 21:06:44 -0400

    Remove commented Spring Security code

--- spring-boot/src/main/java/practice/spring/hello/CustomWebSecurityAdapter.java
+++ spring-boot/src/main/java/practice/spring/hello/CustomWebSecurityAdapter.java
@@ -1,52 +1,17 @@
 package practice.spring.hello;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class CustomWebSecurityAdapter extends WebSecurityConfigurerAdapter {
-
-	/*@Autowired
-	private MyBasicAuthenticationEntryPoint authenticationEntryPoint;*/
-
-	/*@Autowired
-	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication()
-				.withUser("user1").password(passwordEncoder().encode("user1Pass"))
-				.authorities("ROLE_USER");
-	}
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.authorizeRequests()
-				.antMatchers("/securityNone").permitAll()
-				.anyRequest().authenticated()
-				.and()
-				.httpBasic()
-				.authenticationEntryPoint(authenticationEntryPoint);*/
-
-		/*http.addFilterAfter(new CustomFilter(),
-				BasicAuthenticationFilter.class)*/;
-	/*}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}*/
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
