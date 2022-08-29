f7617ec79f56c3f350c65635300848121caa0dd5
Neil Castine <neil.castine@gmail.com>
2022-08-28 21:35:25 -0400

    Only list Beans when "debug" profile is active

--- spring-boot/src/main/java/practice/spring/hello/Application.java
+++ spring-boot/src/main/java/practice/spring/hello/Application.java
@@ -1,12 +1,18 @@
 package practice.spring.hello;
 
 import java.util.Arrays;
+import java.util.concurrent.Callable;
+import java.util.stream.Collectors;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
+import org.springframework.web.context.annotation.RequestScope;
+
+import javax.servlet.http.HttpServletRequest;
 
 @SpringBootApplication
 public class Application {
@@ -16,15 +22,31 @@ public class Application {
     }
 
     @Bean
-    public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    @RequestScope
+    public Callable<String> personalMessage(HttpServletRequest request) {
+        System.out.println("Request scope called " + request);
+        return () -> "Test " + request.getRemoteUser();
+    }
+
+    /**
+     * Registering a command line runner Bean just so it gets run after
+     * everything is wired.
+     */
+    @Bean
+    public CommandLineRunner debugListBeans(ApplicationContext context, Environment environment) {
         return args -> {
-
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
+            // Only print Beans if debug profile is set.
+            // Do not evaluate context until command line runs.
+            if (Arrays.stream(environment.getActiveProfiles()).collect(Collectors.toSet()).contains("debug")) {
+                if (args != null && args.length > 0) {
+                    System.out.println("Arguments: " +  String.join(", ", Arrays.asList(args)));
+                }
+                System.out.println("All registered Beans...");
+                String[] beanNames = context.getBeanDefinitionNames();
+                Arrays.sort(beanNames);
+                for (String beanName : beanNames) {
+                    System.out.println(beanName);
+                }
             }
         };
     }
